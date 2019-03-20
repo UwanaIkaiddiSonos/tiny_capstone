0bc68b89934596a913ab33e38c418152edb709e2
Uwana <uwikaiddi@gmail.com>
2019-03-20 10:53:52 -0500

    Add association between product and supplier

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   # validates :price, numericality: { greater_than: 0 }
   # validates :description, presence: true
   # validates :description, length: { in: 10..500}
+  belongs_to :supplier
 
   
   def is_discounted
