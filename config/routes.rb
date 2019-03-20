97243f180dcf6ff2eff9660dcefa2ea96c3dfda3
Uwana <uwikaiddi@gmail.com>
2019-03-20 06:05:54 -0500

    Add create method and view, uncommented protect_from_forgery

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/products" => "products#index"
-    post "/products" => "products#create"
     get "/products/:id" => "products#show"
+    post "/products" => "products#create"
     get "/products/:id" => "products#update"
     get "/products/:id" => "products#destroy"
 
