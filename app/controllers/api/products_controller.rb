4bcfa54a24cc169f2410568d530f5d82342067b1
Uwana <uwikaiddi@gmail.com>
2019-03-20 05:42:33 -0500

    Add show method and view

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,9 @@ class Api::ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.json.jbuilder"
+  end
 end
