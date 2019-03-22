b9639a4b50640787083318d9a88f1db581296b57
Uwana <uwikaiddi@gmail.com>
2019-03-21 20:40:17 -0500

    Create html controller for product

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  
+  get "/products" => "products#index"
+
   namespace :api do
     # Products
     get "/products" => "products#index"
