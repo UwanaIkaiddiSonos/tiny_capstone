5c46b97fa792e81593a273dd770350d17c039fe1
Uwana <uwikaiddi@gmail.com>
2019-03-21 21:22:36 -0500

    Add new view

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,17 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   
-  get "/products" => "products#index"
-  get "/products/:id" => "products#show"
+  get "/products" => 'products#index'
+  get "products/new" => 'products#new'
+  get "/products/:id" => 'products#show'
 
   namespace :api do
     # Products
-    get "/products" => "products#index"
-    get "/products/:id" => "products#show"
-    post "/products" => "products#create"
-    patch "/products/:id" => "products#update"
-    delete "/products/:id" => "products#destroy"
+    get "/products" => 'products#index'
+    get "/products/:id" => 'products#show'
+    post "/products" => 'products#create'
+    patch "/products/:id" => 'products#update'
+    delete "/products/:id" => 'products#destroy'
 
     #Users
     post "/users" => "users#create"
@@ -24,7 +25,5 @@ Rails.application.routes.draw do
     #CartedProduct
     get "carted_products" => "carted_products#index"
     post "carted_products" => "carted_products#create"
-
   end
-
 end
